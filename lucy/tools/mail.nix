e3fd49c7e05206cc1addbe787c6352512f15228f
Lucy McPhail <lucy.mcphail.p@gmail.com>
2022-09-08 13:44:22 +0100

    Fix SMTP settings

--- lucy/tools/mail.nix
+++ lucy/tools/mail.nix
@@ -22,7 +22,10 @@ in
         address = "lucy.mcphail.p@gmail.com";
 
         imap.host = "imap.gmail.com";
-        smtp.host = "smtp.gmail.com";
+        smtp = {
+          host = "smtp.gmail.com";
+          tls.enable = true;
+        };
         userName = address;
         passwordCommand = "${pass} ${imap.host}/${address}";
 
@@ -32,7 +35,11 @@ in
         address = "L.R.McPhail@sms.ed.ac.uk";
 
         imap.host = "outlook.office365.com";
-        smtp.host = "smtp.office365.com";
+        smtp = {
+          host = "smtp.office365.com";
+          tls.enable = true;
+          tls.useStartTls = true;
+        };
         userName = "s2079454@ed.ac.uk";
         passwordCommand = "${pass} ${imap.host}/${address}";
 
