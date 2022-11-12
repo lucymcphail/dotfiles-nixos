53e3687089a93e3399e7304a9614d2ae66f29729
Lucy McPhail <lucy.mcphail.p@gmail.com>
2022-11-12 11:59:31 +0000

    gpg: add ssh auth key

--- lucy/tools/gpg.nix
+++ lucy/tools/gpg.nix
@@ -6,7 +6,7 @@
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
-    # pinentryFlavor = "gnome3";
+    sshKeys = [ "4010EEE586A8251EA17B0F3296662A42B51F0460" ];
   };
 
   programs =
