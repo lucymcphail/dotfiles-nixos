b2ad26e25520e4e40ffcb81918a94c121df61abb
Lucy McPhail <lucy.mcphail.p@gmail.com>
2022-11-23 03:03:26 +0000

    emacs: fix SSH auth for daemon running under systemd
    Use exec-path-from-shell to get the $SHH_AUTH_SOCK environment
    variable, so that Emacs uses gpg-agent.

--- etc/emacs/lucy-lisp/lucy-essentials.el
+++ etc/emacs/lucy-lisp/lucy-essentials.el
@@ -28,7 +28,8 @@
 
 (use-package exec-path-from-shell
   :config
-  (exec-path-from-shell-initialize))
+  (exec-path-from-shell-initialize)
+  (exec-path-from-shell-copy-env "SSH_AUTH_SOCK"))
 
 (use-package whitespace-cleanup-mode
   :config
